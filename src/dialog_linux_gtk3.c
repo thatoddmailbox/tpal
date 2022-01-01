2207d3b38121091bfc6912e85332200fecf2409a
Alex Studer <alex@studer.dev>
2021-12-31 20:26:45 -0500

    dialog_linx_gtk3: actually return the selected filename

--- src/dialog_linux_gtk3.c
+++ src/dialog_linux_gtk3.c
@@ -7,20 +7,28 @@
 
 #include "dialog_linux.h"
 
+void * libglib2_handle;
 void * libgtk3_handle;
+
+void (*_g_free)(void * memory);
 
 void (*_gtk_init)(int * argc, char *** argv);
 GtkWidget * (*_gtk_file_chooser_dialog_new)(const gchar * title, GtkWindow * parent, GtkFileChooserAction action, const gchar * first_button_text, ...);
+gchar * (*_gtk_file_chooser_get_filename)(GtkFileChooser * chooser);
 gint (*_gtk_dialog_run)(GtkDialog * dialog);
 void (*_gtk_widget_destroy)(GtkWidget * widget);
 
 void tpal_dialog_linux_gtk3_init() {
+	libglib2_handle = dlopen("libglib-2.0.so", RTLD_LAZY);
 	libgtk3_handle = dlopen("libgtk-3.so", RTLD_LAZY);
 
 	// TODO: error handling
 
+	_g_free = dlsym(libglib2_handle, "g_free");
+
 	_gtk_init = dlsym(libgtk3_handle, "gtk_init");
 	_gtk_file_chooser_dialog_new = dlsym(libgtk3_handle, "gtk_file_chooser_dialog_new");
+	_gtk_file_chooser_get_filename = dlsym(libgtk3_handle, "gtk_file_chooser_get_filename");
 	_gtk_dialog_run = dlsym(libgtk3_handle, "gtk_dialog_run");
 	_gtk_widget_destroy = dlsym(libgtk3_handle, "gtk_widget_destroy");
 
@@ -38,9 +46,19 @@ char * tpal_dialog_linux_gtk3_open_file() {
 		GTK_RESPONSE_ACCEPT,
 		NULL
 	);
+
+	char * result_filename = NULL;
 	gint result = _gtk_dialog_run((GtkDialog *) dialog);
+	if (result == GTK_RESPONSE_ACCEPT) {
+		// while gtk_file_chooser_get_filename does give ownership to us, we're supposed to free the memory with g_free
+		// the outside world doesn't know about g_free, so we copy it to a normal allocation that can be freed with free
+		gchar * result_filename_g = _gtk_file_chooser_get_filename((GtkFileChooser *) dialog);
+		result_filename = strdup(result_filename_g);
+		_g_free(result_filename_g);
+	}
+
 	_gtk_widget_destroy(dialog);
-	return NULL;
+	return result_filename;
 }
 
 tpal_dialog_dispatch_t dispatch_linux_gtk3 = {
