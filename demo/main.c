d5644de6b5d6f55af88bf80e4401465451e9b04e
Alex Studer <alex@studer.dev>
2021-12-31 21:23:03 -0500

    demo: call open_file with filter options
    this is a bit messy, is there a better way of setting up the filter structs?

--- demo/main.c
+++ demo/main.c
@@ -10,7 +10,35 @@ int main(int argc, const char * argv[])
 
 	tpal_init();
 
+	//
+	// open file, no filter options
+	//
 	char * result = tpal_dialog_open_file(NULL, NULL);
+	printf("result: %s\n", result);
+	if (result != NULL) {
+		free(result);
+	}
+
+	//
+	// open file, filter options
+	//
+	const char * text_extensions[] = {".txt", NULL};
+	TpalDialogFilter filters[] = {
+		{
+			.name = "Text files",
+			.extensions = text_extensions
+		},
+		{
+			.name = NULL,
+			.extensions = NULL
+		}
+	};
+	TpalDialogFilterOptions options = {
+		.allow_all_files = false,
+		.filters = filters
+	};
+
+	result = tpal_dialog_open_file(NULL, NULL);
 	printf("result: %s\n", result);
 	if (result != NULL) {
 		free(result);
