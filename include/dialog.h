80f31ed50825294e9c47a728b86d8467dfabaf4d
Alex Studer <alex@studer.dev>
2021-12-31 21:20:40 -0500

    include/dialog: define structs for filters

--- include/dialog.h
+++ include/dialog.h
@@ -1,6 +1,18 @@
 #ifndef _TPAL_DIALOG_H
 #define _TPAL_DIALOG_H
 
+#include <stdbool.h>
+
+typedef struct {
+	const char * name;
+	const char ** extensions;
+} TpalDialogFilter;
+
+typedef struct {
+	bool allow_all_files;
+	TpalDialogFilter * filters;
+} TpalDialogFilterOptions;
+
 char * tpal_dialog_open_file(const char * title);
 
 #endif
