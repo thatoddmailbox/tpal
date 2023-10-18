40736fabfd969ad20e5cc0f3731d83cd8b773535
Alex Studer <alex@studer.dev>
2023-10-18 02:39:04 -0400

    on linux, call XInitThreads at startup, which fixes segfaults when using tpal with another UI library that relies on Xlib

--- src/init.c
+++ src/init.c
@@ -1,5 +1,20 @@
 #include "base.h"
 
+#ifdef TPAL_PLATFORM_LINUX
+#include <dlfcn.h>
+#endif
+
 void tpal_init() {
+#ifdef TPAL_PLATFORM_LINUX
+	// need to call XInitThreads, since GTK3 uses Xlib, and our user might also use Xlib
+	// TODO: what we aren't using Xlib? what if the user doesn't even HAVE Xlib?
+	void * libX11_handle = dlopen("libX11.so", RTLD_NOW);
+
+	int (*_XInitThreads)();
+	_XInitThreads = dlsym(libX11_handle, "XInitThreads");
+	_XInitThreads();
+	// note that we do NOT dlclose Xlib - otherwise, we might unlink from it and it goes away, which resets what we just did!
+#endif
+
 	tpal_dialog_init();
 }
