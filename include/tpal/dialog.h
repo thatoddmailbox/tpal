9853b4645e75c60712b1278058a7899951220023
Alex Studer <alex@studer.dev>
2023-10-18 02:12:58 -0400

    include/tpal: fix issue when being included from C++ projects

--- include/tpal/dialog.h
+++ include/tpal/dialog.h
@@ -1,6 +1,10 @@
 #ifndef _TPAL_DIALOG_H
 #define _TPAL_DIALOG_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 #include <stdbool.h>
 
 typedef struct {
@@ -15,5 +19,9 @@ typedef struct {
 
 char * tpal_dialog_open_file(const char * title, TpalDialogFilterOptions * filter_options);
 char * tpal_dialog_save_file(const char * title, TpalDialogFilterOptions * filter_options);
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
--- include/tpal/init.h
+++ include/tpal/init.h
@@ -1,6 +1,14 @@
 #ifndef _TPAL_INIT_H
 #define _TPAL_INIT_H
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
 void tpal_init();
+
+#ifdef __cplusplus
+}
+#endif
 
 #endif
