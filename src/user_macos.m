71a469b3e7ea1b5a8d30c4e814c613c7bbe9f11e
Alex Studer <alex@studer.dev>
2024-04-19 23:34:36 -0400

    user_macos: implement tpal_user_shell_open

--- src/user_macos.m
+++ src/user_macos.m
@@ -2,10 +2,12 @@
 
 #include <string.h>
 
+#include <AppKit/AppKit.h>
+
 char * tpal_user_get_temp_path() {
 	return strdup("/tmp/");
 }
 
 void tpal_user_shell_open(const char * path) {
-	// TODO: implement
+	[[NSWorkspace sharedWorkspace] openFile:[NSString stringWithUTF8String:path]];
 }
